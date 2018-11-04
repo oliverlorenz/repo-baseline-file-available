8848a70e386e34e06b81b6dc9a45d1a177bf09ff
Oliver Lorenz <mail@oliverlorenz.com>
2018-11-04 12:42:37 +0100

    implemented

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const Promise = require('bluebird');
+const fs = require('fs');
+const path = require('path')
+
+module.exports = {
+    run: (repoPath, options, resultCallback) => {
+        let isCheckFullyValid = true;
+        options.files.forEach(fileToCheck => {
+            const fullFilePath = path.join(
+                repoPath,
+                fileToCheck
+            );
+
+            let isFileAvailable = false
+            try {
+                isFileAvailable = fs.statSync(fullFilePath).isFile();
+            } catch (err) { 
+                isCheckFullyValid = false;
+            }
+            resultCallback(`"${fullFilePath}" is available`, isFileAvailable);
+        });
+
+        if (!isCheckFullyValid) {
+            return Promise.reject()
+        }
+        return Promise.resolve();
+    }
+}
