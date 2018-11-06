5a142319d3ff93f6c98e7ba1cd8c08ee8dea53c9
Oliver Lorenz <mail@oliverlorenz.com>
2018-11-06 21:12:46 +0100

    save

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const packageJson = require('../package.json')
 
 module.exports = function(pluginManager, repoPath, config) {
-    function run(repoPath, callback, level, options) {
+    function run(callback, level, options) {
         if (!options) {
             return Promise.reject(new Error('options have to be defined!'));
         }
